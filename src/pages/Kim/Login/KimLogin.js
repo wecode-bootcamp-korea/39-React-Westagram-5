293388da41b6aebc47ca578de21733b88c0d7e2f
leodioKim <kevin8603k@gmail.com>
2022-11-03 16:13:01 +0900

    로그인 페이지(기능 구현)

--- src/pages/Kim/Login/KimLogin.js
+++ src/pages/Kim/Login/KimLogin.js
@@ -1,24 +1,24 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import './KimLogin.scss';
+import { useNavigate } from 'react-router-dom';
 
 function KimLogin() {
-  function LoginEvent() {
-    const id = document.getElementsByClassName('login_id')[0];
-    const pw = document.getElementsByClassName('login_pw')[0];
-    //const login = document.getElementsByClassName('login_btn');
-    pw.addEventListener('keyup', function (e) {
-      const id = document.querySelector('.login_id').value;
-      const password = document.querySelector('.login_pw').value;
-      const btnChange = document.querySelector('.login_btn');
-      if (id.length > 0 && password.length > 0) {
-        btnChange.style.backgroundColor = '#0095F6';
-      } else {
-        btnChange.style.backgroundColor = '#bfdffd';
-      }
-    });
-  }
+  const navi = useNavigate();
+  const [id, inputId] = useState('');
+  const [pw, inputPw] = useState('');
 
+  const loginId = e => {
+    inputId(e.target.value);
+  };
+  const loginPw = e => {
+    inputPw(e.target.value);
+  };
+  const keyDown = e => {
+    if (e.code === 'Enter') {
+      navi('/KimMain');
+    }
+  };
+  const loginValid = id && id.includes('@') && pw && pw.length >= 4;
   return (
     <>
       <div className="all_login">
@@ -27,23 +27,31 @@ function KimLogin() {
         </div>
         <div className="input_id">
           <input
-            onKeyUp={LoginEvent}
+            onChange={loginId}
+            onKeyDown={keyDown}
             className="login_id"
             type="text"
             placeholder="전화번호, 사용자 이름 또는 이메일"
           ></input>
           <input
-            onKeyUp={LoginEvent}
+            onChange={loginPw}
+            onKeyDown={keyDown}
             type="password"
             className="login_pw"
             placeholder="비밀번호"
           ></input>
         </div>
-        <Link to="/KimMain">
-          <div className="login_button">
-            <button className="login_btn">로그인</button>
-          </div>
-        </Link>
+        <div className="login_button">
+          <button
+            onClick={() => {
+              navi('/KimMain');
+            }}
+            disabled={!loginValid ? true : false}
+            className="login_btn"
+          >
+            로그인
+          </button>
+        </div>
         <div className="forget">
           <a href="/">비밀번호를 잊으셨나요?</a>
         </div>
