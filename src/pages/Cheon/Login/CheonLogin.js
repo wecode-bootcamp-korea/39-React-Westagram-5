425a8361036e613fd0ef03e836e99e7686c8e3b7
HACCP92 <111282517+HACCP92@users.noreply.github.com>
2022-11-06 21:14:55 -0800

    수정

--- src/pages/Cheon/Login/CheonLogin.js
+++ src/pages/Cheon/Login/CheonLogin.js
@@ -1,13 +1,28 @@
 import React, { useState } from 'react';
 import './CheonLogin.scss';
 import '../../../styles/reset.scss';
+import { useNavigate } from 'react-router-dom';
 
-function CheonLogin() {
-  const [value, setValue] = useState('');
+const CheonLogin = () => {
+  const [emailInput, setEmailIdInput] = useState('');
+  const [pwInput, setPwInput] = useState('');
+  const [disabled, setdisabled] = useState(true);
+  const navigate = useNavigate();
 
-function handleInput (event) {
-  setValue(event.target.value);
-
+  const handleIdInput = event => {
+    setEmailIdInput(event.target.value);
+  };
+  const handlePwInput = event => {
+    setPwInput(event.target.value);
+  };
+  const btnActive = () => {
+    emailInput.includes('e.g., @') && pwInput.length > 5
+      ? setdisabled(false)
+      : setdisabled(true);
+  };
+  const goToMain = () => {
+    navigate('/cheon-main');
+  };
   return (
     <div className="loginform">
       <header>westargram</header>
@@ -17,22 +32,30 @@ function handleInput (event) {
           name="email"
           className="text-file"
           placeholder="전화번호, 사용자 이름 또는 이메일"
-          value={value}
-          onChange={handleInput}
+          value={emailInput}
+          onChange={handleIdInput}
+          onKeyUp={btnActive}
         />
         <input
-          type="text"
+          type="password"
           name="pw"
           className="text-file"
           placeholder="비밀번호"
-          value={value}
-          onChange={handleInput}
+          value={pwInput}
+          onChange={handlePwInput}
+          onKeyUp={btnActive}
         />
-        <input type="button" value="로그인" className="push-btn" />
+        <input
+          type="button"
+          value="로그인"
+          className="push-btn"
+          disabled={disabled}
+          onClick={goToMain}
+        />
         <p>비밀번호를 잊으셨나요?</p>
       </div>
     </div>
   );
-}
+};
 
 export default CheonLogin;
