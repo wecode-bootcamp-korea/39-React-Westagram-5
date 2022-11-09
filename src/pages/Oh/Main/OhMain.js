e1ae93d4f7df67a8ccdd9c7e356a7da335a2e93b
OHJUHYUNG <juuh.yung5@gmail.com>
2022-11-09 11:22:33 +0900

    Add : Mission 3 - 댓글 기능 구현 중

--- src/pages/Oh/Main/OhMain.js
+++ src/pages/Oh/Main/OhMain.js
@@ -1,6 +1,13 @@
+import React, { useState } from 'react';
 import './OhMain.scss';
 
 function Main() {
+  const [comments, setComments] = useState();
+
+  const onSubmit = () => {
+    console.log('submit');
+  };
+
   return (
     <div className="container">
       <div className="nav_container">
@@ -33,10 +40,17 @@ function Main() {
       <section className="main_section">
         <div className="section_1">
           <div className="user">
-            <img className="user__id__img" src="./images/na.jpeg" />
+            <img
+              className="user__id__img"
+              src="/images/ohjuhyung/snowman.jpeg"
+            />
             <p className="user__id">juuh.yung</p>
           </div>
-          <img className="cat__img" src="./images/cat.jpeg" />
+          <img
+            className="cat__img"
+            alt="cat"
+            src="/images/ohjuhyung/cat.jpeg"
+          />
 
           <div className="bottombox">
             <div className="bottombox__icons">
@@ -51,7 +65,10 @@ function Main() {
             </div>
             <div className="bottombox__likes">
               <div className="bottombox__likes__first">
-                <img className="meerrock__img" src="./images/meerrock.jpeg" />
+                <img
+                  className="meerrock__img"
+                  src="/images/ohjuhyung/meerrock.jpeg"
+                />
                 <p className="bottombox__likes__second">Liked by</p>
                 <p className="bottombox__likes__id">meerrock</p>
                 <p className="bottombox__likes__thrid">and</p>
@@ -86,19 +103,68 @@ function Main() {
         </div>
         <div className="section_2">
           <div className="my__id__box">
-            <img className="my__id" src="./images/snowman.jpeg" />
+            <img className="my__id" src="/images/ohjuhyung/snowman.jpeg" />
             <p className="my__id__name">juuh.yung</p>
           </div>
+
           <div className="story__box">
             <div className="story__box__top">
               <p className="story">story</p>
               <p className="story__see__all">See All</p>
             </div>
+            <div className="story_profile">
+              <div className="story1__box">
+                <div className="story1__border">
+                  <img className="story1" src=" /images/ohjuhyung/puppy.jpeg" />
+                </div>
+                <div className="story1__border__info">
+                  <p className="story1__border__id">niceflim</p>
+                  <p className="story1__time">16분 전</p>
+                </div>
+              </div>
 
-            <div className="story__profile">
+              <div className="story2__box">
+                <div className="story2__border">
+                  <img className="story2" src=" /images/ohjuhyung/puppy.jpeg" />
+                </div>
+                <div className="story2__border__info">
+                  <p className="story2__border__id">niceflim</p>
+                  <p className="story2__time">16분 전</p>
+                </div>
+              </div>
+
+              <div className="story3__box">
+                <div className="story3__border">
+                  <img className="story3" src=" /images/ohjuhyung/puppy.jpeg" />
+                </div>
+                <div className="" story3__border__info>
+                  <p className="story3__border__id">niceflim</p>
+                  <p className="story3__time">16분 전</p>
+                </div>
+              </div>
+
+              <div className="story4__box">
+                <div className="story4__border">
+                  <img className="story4" src=" /images/ohjuhyung/puppy.jpeg" />
+                </div>
+                <div className="story4__border__info">
+                  <p className="story4__border__id">niceflim</p>
+                  <p className="story4__time">16분 전</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="suggestion__box">
+            <div className="suggestion__box__top">
+              <p className="suggestion">suggestion</p>
+              <p className="suggestion__see__all">See All</p>
+            </div>
+
+            <div className="suggestion__profile">
               <div className="flower__box">
                 <div className="flower__border">
-                  <img className="flower" src="./images/flower.jpeg" />
+                  <img className="flower" src="/images/ohjuhyung/puppy.jpeg" />
                 </div>
                 <div className="flower__border__info">
                   <p className="flower__border__id">ppp_p.2.2</p>
@@ -108,9 +174,10 @@ function Main() {
                 </div>
                 <div className="flower__follow">Follow</div>
               </div>
+
               <div className="macaron__box">
                 <div className="macaron__border">
-                  <img className="macaron" src="./images/macaron.jpeg" />
+                  <img className="macaron" src="/images/ohjuhyung/puppy.jpeg" />
                 </div>
                 <div className="macaron__border__info">
                   <p className="macaron__border__id">a.a__</p>
@@ -123,7 +190,7 @@ function Main() {
 
               <div className="puppy__box">
                 <div className="puppy__border">
-                  <img className="puppy" src="./images/puppy.jpeg" />
+                  <img className="puppy" src="/images/ohjuhyung/puppy.jpeg" />
                 </div>
                 <div className="puppy__border__info">
                   <p className="puppy__border__id">three_balls</p>
@@ -133,12 +200,6 @@ function Main() {
                 </div>
                 <div className="puppy__follow">Follow</div>
               </div>
-            </div>
-          </div>
-          <div className="suggestions__box">
-            <div className="suggestions__box__top">
-              <p className="for__you">suggestions For You</p>
-              <p className="suggestions__see__all">See All</p>
             </div>
           </div>
 
